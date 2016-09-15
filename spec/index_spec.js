f3605970cd1f5c5f3648c1765e3e94e546b468b1
Vinson Chuong <vinsonchuong@gmail.com>
2016-09-15 07:44:57 -0700

    Extract file resolver from serve-bin

--- spec/index_spec.js
+++ spec/index_spec.js
@@ -1,7 +1,66 @@
+import register from 'test-inject';
+import Directory from 'directory-helpers';
 import serveStaticAssets from 'serve-static-assets';
 
-describe('serve-static-assets', () => {
-  it('works', () => {
-    expect(serveStaticAssets()).toBe('Hello World!');
+async function read() {
+  return await new Promise((resolve) => {
+    let data = '';
+    this.setEncoding('utf-8');
+    this.on('data', (chunk) => {
+      data += chunk;
+    });
+    this.on('end', () => resolve(data));
   });
+}
+
+const inject = register({
+  project: {
+    setUp: () => new Directory('project'),
+    tearDown: async (project) => await project.remove()
+  }
+});
+
+describe('serve-static-assets', () => {
+  it('returns null for a file that does not exist', inject(async ({project}) => {
+    const request = {
+      url: '/foo.html'
+    };
+    expect(await project::serveStaticAssets(request)).toBe(null);
+  }))
+
+  it('returns metadata for a given file', inject(async ({project}) => {
+    await project.write({
+      'foo.html': `
+        <!doctype html>
+        <meta charset="utf-8">
+      `
+    })
+
+    const request = {
+      url: '/foo.html'
+    };
+    const file = await project::serveStaticAssets(request);
+    expect(file.path).toBe(project.path('foo.html'));
+    expect(file.type).toBe('.html');
+    expect(file.stats).toEqual(await project.stat('foo.html'));
+    expect(await file.stream()::read()).toBe(await project.read('foo.html'));
+  }));
+
+  it('returns metadata for directory/index.html given a directory', inject(async ({project}) => {
+    await project.write({
+      'index.html': `
+        <!doctype html>
+        <meta charset="utf-8">
+      `
+    })
+
+    const request = {
+      url: '/'
+    };
+    const file = await project::serveStaticAssets(request);
+    expect(file.path).toBe(project.path('index.html'));
+    expect(file.type).toBe('.html');
+    expect(file.stats).toEqual(await project.stat('index.html'));
+    expect(await file.stream()::read()).toBe(await project.read('index.html'));
+  }));
 });
--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
-export default function() {
-  return 'Hello World!';
+import * as fs from 'fs';
+import * as path from 'path';
+import * as url from 'url';
+
+export default async function(request) {
+  let filePath, fileStats;
+
+  try {
+    filePath = path.join(
+      this.path(),
+      decodeURIComponent(url.parse(request.url).pathname)
+    );
+
+    if (!this.contains(filePath)) {
+      return null;
+    }
+
+    fileStats = await this.stat(filePath);
+
+    if (fileStats.isDirectory()) {
+      filePath = path.join(filePath, 'index.html');
+      fileStats = await this.stat(filePath);
+    }
+
+    return {
+      path: filePath,
+      type: path.extname(filePath),
+      stats: fileStats,
+      stream: () => fs.createReadStream(filePath)
+    };
+  } catch (error) {
+    return null;
+  }
 }
